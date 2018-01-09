b29c51254f8c2ea7b6f4601bd28ac211f376fb33
shinhaha <akrja4@nate.com>
2018-01-10 00:31:49 +0900

    Prim(Vertices)
    Priority_queue by Vertices

--- Algorithm_Note/Minimum_Spanning_Tree/Prim(Vertices).cpp
+++ Algorithm_Note/Minimum_Spanning_Tree/Prim(Vertices).cpp
@@ -3,47 +3,40 @@
 #include <queue>
 #include <functional>
 using namespace std;
-#define ver pair<int,int>
-struct Edge {
-	int dest;
-	int weight;
-};
-#define edge struct Edge
-
+#define ver pair<int,int>//Verweight,source;
+#define edge pair<int,int>//dest,Edgeweight;
+#define INF 1e9
 int main() {
 	int v, e; scanf("%d %d", &v, &e);
 	bool* visit = new bool[v]();
 	int* key = new int[v]();
-	priority_queue<ver, vector<ver>, greater<ver>> pq;
+	priority_queue<ver, vector<ver>, greater<ver> > pq;
 	vector<edge>* Vec = new vector<edge>[v]();
-	for (int i = 0; i < v; i++)key[i] = 2000000000;
+	for (int i = 0; i < v; i++) {
+		visit[i] = false; key[i] = INF;
+	}
 	for (int i = 0; i < e; i++) {
 		int a, b, c; scanf("%d %d %d", &a, &b, &c);
-		edge* tmp1 = new edge(); edge* tmp2 = new edge();
-		tmp1->dest = b - 1; tmp2->dest = a - 1; tmp1->weight = c; tmp2->weight = c;
-		Vec[a - 1].push_back(*tmp1); Vec[b - 1].push_back(*tmp2);
+		Vec[a - 1].push_back(make_pair(b - 1, c)); Vec[b - 1].push_back(make_pair(a - 1, c));
 	}
-	int sum = 0;
-	int size = 0;
-	key[0] = 0;
-	pq.push(make_pair(0, 0));
+	int ret = 0; int size = 0; int src = 0; key[0] = 0;
+	pq.push(make_pair(0,src));//weight,source
 	while (!pq.empty()) {
-		int vertices = pq.top().second;//vertices
+		int weight = pq.top().first;//VerWeight
+		int vertices = pq.top().second;//source
 		pq.pop();
 		if (visit[vertices])continue;
 		visit[vertices] = true;
-		sum += key[vertices];
-		size++;
-		if (size == v)break;
+		ret += weight;
+		size++; if (size == v)break;
 		for (int i = 0; i < Vec[vertices].size(); i++) {
-			edge tmp = Vec[vertices][i];
-			int dest = tmp.dest;
-			int w = tmp.weight;
-			if (!visit[dest] && key[dest] > w) {
-				key[dest] = w;
-				pq.push(make_pair(key[dest], dest));
+			int dest = Vec[vertices][i].first;
+			int Edgeweight = Vec[vertices][i].second;
+			if (!visit[dest] && key[dest] > Edgeweight) {
+				key[dest] = Edgeweight;
+				pq.push(make_pair(Edgeweight, dest));
 			}
 		}
 	}
-	printf("%d", sum);
+	printf("%d", ret);
 }
