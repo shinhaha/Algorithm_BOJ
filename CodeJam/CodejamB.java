9c8f52214a96c895679b18cc23943991d61d39a9
shinhaha <akrja4@nate.com>
2018-01-09 23:53:19 +0900

    2017CodeJamB
    2017CodeJamB

---
+++ CodeJam/CodejamB.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CodejamB {
+	public void Code(){
+		Scanner sc=new Scanner(System.in);
+		String T=sc.nextLine();
+		int X=Integer.parseInt(T);
+		int count=0;
+
+		while(count<X){
+			String S=sc.nextLine();
+			int[] a=new int[S.length()];//문자길이만큼 길이의 배열만듬
+			Arrays.fill(a,0);
+			for(int x=0; x<a.length; x++){
+				a[x]=Integer.parseInt(S.substring(x,x+1));
+			}//배열에 잘라서 넣음.
+
+			for(int i=a.length-1; i>0; i--){
+				if(a[i-1]<=a[i]&&a[i]!=0&&a[i-1]!=0)//오름차순이면
+					continue;
+				else if (a[i]==-1){
+					a[i]=9;
+					a[i-1]--;
+					i=i+1;}
+				else if (a[i-1]==-1){
+					a[i-1]=9;
+					i=i+1;}
+				else if (a[i-1]>a[i]||a[i-1]==0||a[i]==0){//오름차순아니면
+					if(i==1&&a[0]==0)//예외
+						break;
+					if(a[i]!=0){//a[i] 0이 아니면
+						a[i]--;
+						i=a.length;
+					}
+					else{//a[i] 0이면
+						a[i]=9;
+						a[i-1]--;
+						i=a.length;
+					}
+				}
+			}
+			System.out.print("Case #"+(count+1)+": ");
+			for(int p=0; p<a.length; p++){
+				if(a[0]==0&&p==0)
+					continue;
+				System.out.print(a[p]);
+			}
+			System.out.println();
+			count++;
+		}
+	}
+
+	public static void main(String[] args) {
+		CodejamB a=new CodejamB();
+		a.Code();
+	}
+}
