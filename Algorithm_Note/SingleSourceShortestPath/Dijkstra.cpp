13f9dfcdec27803971a7b3e7df1cf2025c2866ad
shinhaha <akrja4@nate.com>
2018-01-21 00:15:58 +0900

    Dijkstra
    Dijkstra

--- Algorithm_Note/SingleSourceShortestPath/Dijkstra.cpp
+++ Algorithm_Note/SingleSourceShortestPath/Dijkstra.cpp
@@ -1,32 +1,32 @@
 //boj 1753
 #include <cstdio>
-#include <queue>
 #include <vector>
+#include <queue>
 #include <functional>
 using namespace std;
-#define PI pair<int,int>//dest,weight
-#define INF 123456789
+#define Vertice pair<int,int>//weight,vertice
+#define Edge pair<int,int>//dest,weight
+#define INF 1e9
 
 int main() {
-	int V, E; scanf("%d %d", &V, &E);
-	int* dist = new int[V+1]();
-	vector<PI>* v = new vector<PI>[V+1]();
-	priority_queue<int,vector<int>,greater<int>> pq;
-	int start; scanf("%d", &start);
-	for (int i = 1; i <= V; i++)
-		dist[i] = INF;
+	int V, E,src; scanf("%d %d %d", &V, &E, &src);
+	priority_queue<Vertice, vector<Vertice>, greater<Vertice>> pq;
+	vector<Edge>* v = new vector<Edge>[V + 1]();
+	int* dist = new int[V + 1](); fill(dist,dist + V + 1,INF);
+
 	for (int i = 0; i < E; i++) {
-		int q ,w, e; scanf("%d %d %d", &q, &w, &e);
-		v[q].push_back({ w,e });
-	}
-	dist[start] = 0;
-	pq.push(start);
+		int from, to, weight; scanf("%d %d %d", &from, &to, &weight);
+		v[from].push_back({to,weight});}
+
+	dist[src] = 0;	pq.push({ dist[src],src });
 	while (!pq.empty()) {
-		int curr = pq.top(); pq.pop();
-		for(int i=0; i<v[curr].size(); i++)
+		int weight = pq.top().first;
+		int curr = pq.top().second; pq.pop();
+		if (dist[curr] < weight)continue;
+		for (int i = 0; i<v[curr].size(); i++)
 			if (dist[v[curr][i].first] > dist[curr] + v[curr][i].second) {
 				dist[v[curr][i].first] = dist[curr] + v[curr][i].second;
-				pq.push(v[curr][i].first);
+				pq.push({ dist[v[curr][i].first],v[curr][i].first });
 			}
 	}
 	for (int i = 1; i <= V; i++) {
