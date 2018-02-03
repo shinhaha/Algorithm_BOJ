41988f8d7ab9e32a9d224b26ffa9c87a4c7cd49c
shinhaha <akrja4@nate.com>
2018-02-03 23:30:13 +0900

    12015

---
+++ BOJ/12015/main.cpp
@@ -0,0 +1,16 @@
+//boj 12015
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+	int n; scanf("%d", &n);
+	vector<int> v;
+	for (int i = 0; i < n; i++) {
+		int input; scanf("%d", &input);
+		auto iter = lower_bound(v.begin(), v.end(), input);
+		if (iter == v.end())v.push_back(input);
+		else *iter = input;
+	}
+	printf("%d", v.size());
+}
