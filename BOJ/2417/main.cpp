fb3ca12db2c410093a5e0030ea798c2d85867cbd
shinhaha <akrja4@nate.com>
2018-04-06 23:55:03 +0900

    2417
    2417

---
+++ BOJ/2417/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <vector>
+#include <string.h>
+#include <string>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int main() {
+	long long n; scanf("%lld", &n);
+	printf("%lld", (long long)ceil(sqrt(n)));
+}
