222727b97b03da7a64fa18ae1ec817252c0d2974
shinhaha <akrja4@nate.com>
2018-04-19 23:11:59 +0900

    10926
    10926

---
+++ BOJ/10926/main.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS // scanf(), gets() (needed for Visual C++)
+#include <cassert>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+using namespace std;
+#define MEMSET(x, WITH) memset(x, (WITH), sizeof(x))
+#define FOR(i, E) for(int i=0; i<(E); i++)
+#define REP(i, LO, HI) for(int i=(LO); i<=(HI); i++)
+#define SORT(v) sort(v.begin(),v.end())
+#define PI pair<int,int>
+typedef unsigned long long ll;
+
+char buf[51];
+int main() {
+	scanf("%s", buf);
+	printf("%s??!", buf);
+}
