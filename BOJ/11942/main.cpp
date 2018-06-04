75c2567b6a86a7a7eb07c081c405c37abd722c69
shinhaha <akrja4@nate.com>
2018-06-04 21:44:16 +0900

    11942
    11942

---
+++ BOJ/11942/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+#include <queue>
+#include <functional>
+using namespace std;
+
+int main() {
+	printf("고려대학교");
+}
