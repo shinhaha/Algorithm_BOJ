0cc0c4a03ccbf9e5ebad8e0c927fd72b122e4fb4
shinhaha <akrja4@nate.com>
2018-07-08 13:43:32 +0900

    1668
    1668

--- BOJ/1668/main.cpp
+++ BOJ/1668/main.cpp
@@ -15,23 +15,17 @@ typedef long long ll;
 
 int main() {
 	int n; scanf("%d", &n);
-	vector<int> v,lc,rc;
+	vector<int> v;
 	for (int i = 0; i < n; i++) {
 		int input; scanf("%d", &input);
 		v.push_back(input);
-		lc.push_back(0), rc.push_back(0);
 	}
 	int retl = 1; int retr = 1;
-
 	int lm = v[0];  int rm = v[n - 1];
 	for (int i = 1; i < n; i++) {
 		if (lm < v[i])retl++, lm = v[i];
 		if (rm < v[n - 1 - i])retr++,rm=v[n-1-i];
 	}
-
 	printf("%d\n%d", retl, retr);
-
 	return 0;
-
-
 }
